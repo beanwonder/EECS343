34d568767af24cce9d5d82dd3be062f6f4037cd6
beanwonder <shenzhebin8@gmail.com>
2016-10-12 14:05:25 -0500

    part1, use lab machine do the test, TODO syscall arguments check stuff 0x0 check

--- user/makefile.mk
+++ user/makefile.mk
@@ -18,6 +18,7 @@ USER_PROGS := \
 	wc\
 	zombie\
 	ps\
+	usertests_2_1
 
 USER_PROGS := $(addprefix user/, $(USER_PROGS))
 
@@ -74,7 +75,7 @@ USER_LDFLAGS += --omagic
 USER_LDFLAGS += --entry=main
 
 # location in memory where the program will be loaded
-USER_LDFLAGS += --section-start=.text=0x0
+USER_LDFLAGS += --section-start=.text=0x1000
 
 user/bin:
 	mkdir -p user/bin
